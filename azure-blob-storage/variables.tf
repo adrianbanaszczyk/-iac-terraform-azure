f74f2afeb625227f3172f7391ba4f37f0ea8b461
adrianbanaszczyk <132902514+adrianbanaszczyk@users.noreply.github.com>
2023-05-23 16:53:41 +0200

    Create variables.tf

---
+++ azure-blob-storage/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type    = string
+  default = "my-resource-group"
+}
+
+variable "resource_group_location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "storage_account_name" {
+  type    = string
+  default = "my-storage-account"
+}
+
+variable "storage_account_tier" {
+  type    = string
+  default
+}
