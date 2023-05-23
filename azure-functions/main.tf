65abda6175c9578de525e1f289573f9182113240
adrianbanaszczyk <132902514+adrianbanaszczyk@users.noreply.github.com>
2023-05-23 16:38:49 +0200

    Create main.tf

---
+++ azure-functions/main.tf
@@ -0,0 +1,44 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_linux_function_app" "example" {
+  name                      = var.function_app_name
+  location                  = azurerm_resource_group.example.location
+  resource_group_name       = azurerm_resource_group.example.name
+  app_service_plan_id       = azurerm_app_service_plan.example.id
+  storage_account_name      = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  version                   = "~3"
+  os_type                   = "linux"
+}
+
+resource "azurerm_function_app_function" "example" {
+  name              = var.function_name
+  resource_group_name  = azurerm_resource_group.example.name
+  function_app_name = azurerm_linux_function_app.example.name
+  storage_account_name = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME" = "node"
+  }
+}
